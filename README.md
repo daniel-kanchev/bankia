6022e95ee85c5a444017a5623c4515da9edec373
daniel-kanchev <daniel.kanchev@adata.pro>
2021-03-05 12:15:40 +0200

    Finished Script

---
+++ README.md
@@ -0,0 +1,11 @@
+URL: https://www.bankia.com/en/communication/news/
+
+    Spider name: bankia
+
+DB Schema:
+- title
+- date
+- link
+- content
+
+Notes:
---
+++ bankia/settings.py
@@ -0,0 +1,13 @@
+BOT_NAME = 'bankia'
+SPIDER_MODULES = ['bankia.spiders']
+NEWSPIDER_MODULE = 'bankia.spiders'
+USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
+ITEM_PIPELINES = {
+    'bankia.pipelines.DatabasePipeline': 300,
+}
+FEED_EXPORT_ENCODING = 'utf-8'
+ROBOTSTXT_OBEY = True
+
+LOG_LEVEL = 'WARNING'
+
+# LOG_LEVEL = 'DEBUG'
